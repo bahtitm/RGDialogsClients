2be928fe10965f9e5020f1b1f05b8c5713a56f25
bahtitm <bahti1986@gmail.com>
2021-08-18 22:05:47 +0500

    EndTask

---
+++ RGDialogsClients/Controllers/RGDialogsClientController.cs
@@ -0,0 +1,39 @@
+﻿using GetDialog.Entitties;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetDialog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RGDialogsClientController : ControllerBase
+    {
+
+
+        private readonly ILogger<RGDialogsClientController> _logger;
+        private List<RGDialogsClients> _rgDialogsClients;
+
+        public RGDialogsClientController(ILogger<RGDialogsClientController> logger)
+        {
+            _logger = logger;
+            var rgDialogsClients = new RGDialogsClients();
+            _rgDialogsClients = rgDialogsClients.Init();
+        }
+
+        [HttpPost]
+        public Guid GetDialog(IEnumerable<Guid> idClients)
+        {
+            var result = idClients.Where(p => _rgDialogsClients.Any(a => a.IDClient.Equals(p)));
+            if (result != null)
+            {
+                return result.FirstOrDefault();
+            }
+
+
+            return Guid.Empty;
+        }
+    }
+}
--- RGDialogsClients/Startup.cs
+++ RGDialogsClients/Startup.cs
@@ -1,18 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace RGDialogsClients
+namespace GetDialog
 {
     public class Startup
     {
@@ -23,7 +16,7 @@ namespace RGDialogsClients
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+      
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +27,7 @@ namespace RGDialogsClients
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -47,8 +40,6 @@ namespace RGDialogsClients
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
